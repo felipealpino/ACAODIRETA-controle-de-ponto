3af3afd17e999a469a6c40f851be931dcdf4f21b
Felipe Alpino <felipealpino1@gmail.com>
2021-02-07 18:21:31 -0300

    Primeiro commit - iniciando login

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/teste_acaoDireta/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'acaodireta';
+    CONST DB_USER = 'root';
+    const DB_PASS = '';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
---
+++ src/controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class HomeController extends Controller {
+
+    private $loggedUser; 
+
+    public function __construct(){
+        
+    }
+
+    public function index() {
+        $this->render('home', ['nome' => 'Bonieky']);
+    }
+
+    public function sobre() {
+        $this->render('sobre');
+    }
+
+    public function sobreP($args) {
+        print_r($args);
+    }
+
+}
